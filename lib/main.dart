e8222905e2ddc8b3edd04c71052de6a830d5e708
nkwook <ruddnrdl@kaist.ac.kr>
2021-09-27 17:19:21 +0900

    Simple Navigating structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'src/home.dart';
-import 'src/today.dart';
+import 'src/bottomNavigator.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '티끌리',
-      home: BottomNavigator(),
+      home: Login(),
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
@@ -17,42 +16,84 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class BottomNavigator extends StatelessWidget {
-  // TabsState createState() => TabsState();
+class Login extends StatefulWidget {
+  LoginState createState() => LoginState();
+}
+
+class LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> _widgetOptions = [
-      Home(),
-      Today(),
-      Placeholder(),
-    ];
-
-    return DefaultTabController(
-        initialIndex: 1,
-        length: 3,
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text('티끌리'),
-            ),
-            bottomNavigationBar: TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.nature),
-                  height: 120,
-                ),
-                Tab(
-                  icon: Icon(Icons.nature),
-                ),
-                Tab(
-                  icon: Icon(Icons.settings),
-                ),
-              ],
-              indicatorColor: Colors.transparent,
-              unselectedLabelColor: Colors.grey,
-              labelColor: Colors.black,
-            ),
-            body: TabBarView(
-              children: _widgetOptions,
-            )));
+    return Scaffold(
+      body: _loginScreen(),
+    );
   }
+
+  Widget _loginScreen() {
+    return Center(
+      child: Column(
+        children: <Widget>[_appLogo(), _loginForm()],
+      ),
+    );
+  }
+
+  Widget _appLogo() {
+    return Container(
+        height: 200,
+        margin: EdgeInsets.only(top: 200, bottom: 100, left: 40, right: 40),
+        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/logo.png'), fit: BoxFit.fill)));
+  }
+
+  Widget _loginForm() {
+    var name;
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        // height: 400.0,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  name = value;
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState!.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('로그인 되었습니다')),
+                      );
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  BottomNavigator(name: name)));
+                    }
+                  },
+                  child: const Text('Login'),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  // Widget
 }
